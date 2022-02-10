7a18f3a497f9b50dae40f8cb778fc6c41abd09ed
uqba <muhammmad.uqba@codilitysolutions.com>
2022-02-10 15:27:58 +0500

    signup nai ho raha the, wo sahi kiya hai

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by_email(params[:email])
+        @user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
-        if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+        if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       @message = "ok"
       redirect_to root_path
         else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
 
     def new
-        
+        @user = User.new
     end
 
     def create
-        user = User.new(user_params)
-        if user.save
-            session[:user_id] = user.id
-            redirect_to 'users/login'
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to '/users/login'
         else
             redirect_to 'users/signup'
         end
